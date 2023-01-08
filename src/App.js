f8f4d47b5860e7103beb3e5bb80a906faa91c1ad
tugce <tugceozdemiir@hotmail.com>
2023-01-08 23:30:42 +0300

    tugce

--- src/App.js
+++ src/App.js
@@ -1,61 +1,86 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import FavoriteMovieList from './components/FavoriteMovieList';
+import FavoriteMovieList from "./components/FavoriteMovieList";
+import EditMovieForm from "./components/EditMovieForm";
 
-import axios from 'axios';
+import axios from "axios";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:9000/api/movies')
-      .then(res => {
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/api/movies")
+      .then((res) => {
         setMovies(res.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  const deleteMovie = (id)=> {
-  }
+  const deleteMovie = (id) => {
+    axios
+      .delete("http://localhost:9000/api/movies/" + id)
+      .then((res) => {
+        setMovies(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const addToFavorites = (movie) => {
-    
-  }
+    const favoriteList = favoriteMovies.filter((fav) => fav.id === movie.id)[0];
+    !favoriteList && setFavoriteMovies([...favoriteMovies, movie]);
+  };
+
+  const removeFavorites = (movie) => {
+    const favoriteList = favoriteMovies.filter((fav) => fav.id !== movie.id);
+    setFavoriteMovies(favoriteList);
+  };
 
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" > HTTP / CRUD Module Project</span>
+        <span className="navbar-brand"> HTTP / CRUD Module Project</span>
       </nav>
 
       <div className="container">
-        <MovieHeader/>
+        <MovieHeader />
         <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
-        
+          <FavoriteMovieList favoriteMovies={favoriteMovies} />
+
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm movies={movies} setMovies={setMovies} />
+            </Route>
+
+            <Route path="/movies/add">
+              <EditMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie
+                deleteMovie={deleteMovie}
+                addToFavorites={addToFavorites}
+                removeFavorites={removeFavorites}
+              />
             </Route>
 
             <Route path="/movies">
-              <MovieList movies={movies}/>
+              <MovieList movies={movies} />
             </Route>
 
             <Route path="/">
-              <Redirect to="/movies"/>
+              <Redirect to="/movies" />
             </Route>
           </Switch>
         </div>
@@ -64,6 +89,4 @@ const App = (props) => {
   );
 };
 
-
 export default App;
-
--- src/components/EditMovieForm.js
+++ src/components/EditMovieForm.js
@@ -1,79 +1,135 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
 
 const EditMovieForm = (props) => {
-	const { push } = useHistory();
+  const { push } = useHistory();
+  const { id } = useParams();
 
-	const { setMovies } = props;
-	const [movie, setMovie] = useState({
-		title:"",
-		director: "",
-		genre: "",
-		metascore: 0,
-		description: ""
-	});
-	
-	const handleChange = (e) => {
-        setMovie({
-            ...movie,
-            [e.target.name]: e.target.value
-        });
-    }
+  const { setMovies } = props;
+  const [movie, setMovie] = useState({
+    title: "",
+    director: "",
+    genre: "",
+    metascore: 0,
+    description: "",
+  });
+  //console.log(id);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.put(`http://localhost:9000/api/movies/${id}`, movie)
-            .then(res=>{
-                setMovies(res.data);
-                push(`/movies/${movie.id}`);
-			})
-			.catch(err=>{
-				console.log(err);
-			})
-	}
-	
-	const { title, director, genre, metascore, description } = movie;
+  useEffect(() => {
+    axios
+      .get(`http://localhost:9000/api/movies/${id}`)
+      .then((res) => setMovie(res.data))
+      .catch((err) => console.log(err));
+  }, [id]);
 
-    return (
-	<div className="col">
-		<div className="modal-content">
-			<form onSubmit={handleSubmit}>
-				<div className="modal-header">						
-					<h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
-				</div>
-				<div className="modal-body">					
-					<div className="form-group">
-						<label>Title</label>
-						<input value={title} onChange={handleChange} name="title" type="text" className="form-control"/>
-					</div>
-					<div className="form-group">
-						<label>Director</label>
-						<input value={director} onChange={handleChange} name="director" type="text" className="form-control"/>
-					</div>
-					<div className="form-group">
-						<label>Genre</label>
-						<input value={genre} onChange={handleChange} name="genre" type="text" className="form-control"/>
-					</div>
-					<div className="form-group">
-						<label>Metascore</label>
-						<input value={metascore} onChange={handleChange} name="metascore" type="number" className="form-control"/>
-					</div>		
-					<div className="form-group">
-						<label>Description</label>
-						<textarea value={description} onChange={handleChange} name="description" className="form-control"></textarea>
-					</div>
-									
-				</div>
-				<div className="modal-footer">			    
-					<input type="submit" className="btn btn-info" value="Save"/>
-					<Link to={`/movies/1`}><input type="button" className="btn btn-default" value="Cancel"/></Link>
-				</div>
-			</form>
-		</div>
-	</div>);
-}
+  const handleChange = (e) => {
+    setMovie({
+      ...movie,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios({
+      method: id ? "put" : "post",
+      url: id
+        ? "http://localhost:9000/api/movies/" + id
+        : "http://localhost:9000/api/movies/",
+      data: movie,
+    })
+      .then((res) => {
+        setMovies(res.data);
+        console.log(res.data);
+        push(`/movies`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const { title, director, genre, metascore, description } = movie;
+
+  return (
+    <div className="col">
+      <div className="modal-content">
+        <form onSubmit={handleSubmit}>
+          <div className="modal-header">
+            {id ? (
+              <h4 className="modal-title">
+                Editing <strong>{movie.title}</strong>
+              </h4>
+            ) : (
+              <h4 className="modal-title">
+                <strong>Add New Movie</strong>
+              </h4>
+            )}
+          </div>
+          <div className="modal-body">
+            <div className="form-group">
+              <label>Title</label>
+              <input
+                value={title}
+                onChange={handleChange}
+                name="title"
+                type="text"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Director</label>
+              <input
+                value={director}
+                onChange={handleChange}
+                name="director"
+                type="text"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Genre</label>
+              <input
+                value={genre}
+                onChange={handleChange}
+                name="genre"
+                type="text"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Metascore</label>
+              <input
+                value={metascore}
+                onChange={handleChange}
+                name="metascore"
+                type="number"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Description</label>
+              <textarea
+                value={description}
+                onChange={handleChange}
+                name="description"
+                className="form-control"
+              ></textarea>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <input type="submit" className="btn btn-info" value="Save" />
+            <Link to={`/movies/1`}>
+              <input type="button" className="btn btn-default" value="Cancel" />
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default EditMovieForm;
